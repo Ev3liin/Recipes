555c3faec093e08249e8e1802c0c061d833fafed
Evelin <evelinzirakashvili11@gmail.com>
2021-02-12 00:19:40 +0400

    - dertails page
    not finished

---
+++ src/views/Details.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- <p>{{ recipe.name }}</p> -->
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+
+export default {
+  props: ['id'],
+  setup(props) {
+    const store = useStore()
+    const recipe = computed(() => {
+      return store.getters.getById(props.id)
+    })
+
+    console.log(recipe)
+    return { recipe }
+  },
+}
+</script>
+
+<style></style>
