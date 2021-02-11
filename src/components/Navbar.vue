b68323d6530f871a1c09d955c442a52d2acb5cf9
Evelin <evelinzirakashvili11@gmail.com>
2021-02-12 00:10:55 +0400

    - navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="header">
+    <router-link class="logo" :to="{ name: 'Home' }">Recipes</router-link>
+    <nav>
+      <router-link :to="{ name: 'Home' }">Home</router-link>
+      <router-link :to="{ name: 'Create' }">Create Recipe</router-link>
+    </nav>
+  </header>
+</template>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 10px;
+}
+.logo {
+  color: #dfdfdf !important;
+  font-size: 48px;
+  font-weight: bold !important;
+}
+
+.header a {
+  color: #bbb;
+  text-decoration: none;
+  margin-left: 20px;
+}
+.header a.router-link-active {
+  color: #ff8800;
+  font-weight: bold;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home'
+import Create from '../views/Create'
+import Details from '../views/Details'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/recipes/create',
+    name: 'Create',
+    component: Create,
+  },
+  {
+    path: '/recipes/:id',
+    name: 'Details',
+    component: Details,
+    props: true,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 })
 
 export default router
