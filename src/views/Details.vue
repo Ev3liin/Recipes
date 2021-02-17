bf7a55f48c626bea121f88e8cbba7c09041dff05
Evelin <evelinzirakashvili11@gmail.com>
2021-02-17 16:45:28 +0400

    Rectify single recipe' Details page

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,30 +1,43 @@
 <template>
-  <h2 class="title">{{ recipe.name }}</h2>
-  <div class="d-flex">
-    <img class="img" :src="recipe.image" :alt="recipe.name" />
-    <div>
-      <p class="description-p">{{ recipe.description }}</p>
-      <p class="description-p ">
-        Time to Prepare: {{ recipe.time_to_prepare }} minutes
-      </p>
+  <div v-if="recipe" class="recipe details">
+    <h2 class="title">{{ recipe.name }}</h2>
+    <div class="d-flex">
+      <img class="img" :src="recipe.image" :alt="recipe.name" />
+      <div>
+        <p class="description-p">{{ recipe.description }}</p>
+        <p class="description-p ">
+          Time to Prepare: {{ recipe.time_to_prepare }} minutes
+        </p>
+      </div>
     </div>
+    <router-link
+      class="update-link"
+      :to="{ name: 'Update', params: { id: recipe.id } }"
+    >
+      <span class="btn btn-update">Update Recipe</span>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      recipe: {},
-    }
+  props: ['id'],
+  computed: {
+    recipe() {
+      return this.$store.state.recipes.recipe
+    },
   },
   mounted() {
-    fetch(`http://localhost:3000/recipes/${this.$route.params.id}`)
-      .then(res => res.json())
-      .then(data => (this.recipe = data))
-      .catch(err => console.log(err.message))
+    this.$store.dispatch('recipes/getRecipe', this.id)
   },
 }
 </script>
 
-<style></style>
+<style>
+.details {
+  width: 1000px !important;
+}
+.update-link {
+  text-decoration: none;
+}
+</style>
