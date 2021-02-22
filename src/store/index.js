977aed513f85b7f260abc2efb536c630b6a79a0c
Evelin <evelinzirakashvili11@gmail.com>
2021-02-22 18:35:29 +0400

    Clear store from needless comments

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,7 @@
 import { createStore } from 'vuex'
 import recipesModule from './modules/recipes'
-export default createStore({
-  // state: {
-  //   recipes: [],
-  // },
-  // mutations: {
-  //   SET_RECIPES(state, recipes) {
-  //     state.recipes = recipes
-  //   },
-  //   ADD_RECIPE(state, recipe) {
-  //     state.recipes.push(recipe)
-  //   },
-  // },
-  // actions: {
-  //   fetchRecipes({ commit }) {
-  //     fetch('http://localhost:3000/recipes')
-  //       .then(res => res.json())
-  //       .then(data => commit('SET_RECIPES', data))
-  //       .catch(err => console.log(err.message))
-  //   },
-  // },
-  // getters: {
 
-  // },
+export default createStore({
   modules: {
     recipes: recipesModule,
   },
