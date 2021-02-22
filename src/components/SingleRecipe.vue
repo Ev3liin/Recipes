5a8d4dacfdb07d6b297789e3e5fad5e7bfb2df29
Evelin <evelinzirakashvili11@gmail.com>
2021-02-22 18:33:47 +0400

    Add descriptions' snippet to the recipes's list

--- src/components/SingleRecipe.vue
+++ src/components/SingleRecipe.vue
@@ -1,8 +1,12 @@
 <template>
-  <router-link :to="{ name: 'Details', params: { id: recipe.id } }">
+  <router-link
+    class="details-link"
+    :to="{ name: 'Details', params: { id: recipe.id } }"
+  >
     <div class="recipe">
       <img class="img" :src="recipe.image" :alt="recipe.name" />
-      <h3>{{ recipe.name }}</h3>
+      <h3 class="title">{{ recipe.name }}</h3>
+      <p class="description">{{ snippet }}</p>
     </div>
   </router-link>
 </template>
@@ -10,6 +14,11 @@
 <script>
 export default {
   props: ['recipe'],
+  computed: {
+    snippet() {
+      return this.recipe.description.substring(0, 200) + '...'
+    },
+  },
 }
 </script>
 
@@ -18,11 +27,22 @@ export default {
   margin: 20px auto;
   height: auto;
   min-width: 300px;
+  max-width: 500px;
   background: #ffebde;
   border-radius: 20px;
   box-shadow: 5px 10px 15px #ccc;
   padding: 30px;
   transition-duration: 0.8px;
+}
+.recipe .description {
+  color: #4e2906;
+}
+.recipe .title {
+  color: #ff8800;
+  font-size: 25px;
+}
+.details-link {
+  text-decoration: none;
 }
 .recipe:hover {
   box-shadow: 5px 10px 15px #a7a2a2;
